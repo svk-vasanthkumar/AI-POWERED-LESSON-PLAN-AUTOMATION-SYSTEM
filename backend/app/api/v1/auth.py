9e7bc5730385aff4da3d824bc0c78f5efc3245ef
svk-vasanthkumar <vasanth636807@gmail.com>
2026-08-19 13:55:52 +0530

    feat: add authentication API

---
+++ backend/app/api/v1/auth.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from app.auth.dependencies import get_current_user
+from app.schemas.user_schema import UserLogin, UserRegister
+from app.services.auth_service import login_user, register_user
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+
+@router.post("/register")
+async def register(user: UserRegister):
+    try:
+        return await register_user(user)
+
+    except ValueError as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
+
+
+@router.post("/login")
+async def login(user: UserLogin):
+    try:
+        return await login_user(
+            user.email,
+            user.password,
+        )
+
+    except ValueError as e:
+        raise HTTPException(
+            status_code=401,
+            detail=str(e),
+        )
+
+
+@router.get("/profile")
+async def profile(user=Depends(get_current_user)):
+    return {
+        "message": "Authorized",
+        "user": user,
+    }
