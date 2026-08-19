4a073f602598c08f258e20de32e1d4b9e375894f
svk-vasanthkumar <vasanth636807@gmail.com>
2026-08-19 13:55:55 +0530

    feat: add course model

---
+++ backend/app/models/course_model.py
@@ -0,0 +1,23 @@
+from datetime import datetime, UTC
+
+from bson import ObjectId
+
+
+def create_course_document(
+    course_code: str,
+    course_name: str,
+    department: str,
+    semester: int,
+    credits: int,
+    faculty_id: ObjectId,
+):
+    return {
+        "course_code": course_code.upper(),
+        "course_name": course_name,
+        "department": department,
+        "semester": semester,
+        "credits": credits,
+        "faculty_id": faculty_id,
+        "created_at": datetime.now(UTC),
+        "updated_at": datetime.now(UTC),
+    }
